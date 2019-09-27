1ad4c8e52ba93e18ef41c7ce7843904e8a1ea435
Harald Albrecht <hidden@hidden.org>
2019-09-27 19:21:09 +0200

    adds example

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,42 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/moby/moby/pkg/reexec"
 
 	_ "github.com/thediveo/gons"
 )
 
+// Thanks to
+// https://jiajunhuang.com/articles/2018_08_28-how_does_golang_implement_fork_syscall.md.html
+// for the example of how to use Moby's reexec to restart ourself in order to
+// be able to switch into prickly namespaces, such as the mount namespace: it
+// cannot be changed after the Go runtime has spun up and created additional
+// OS threads.
+func init() {
+	reexec.Register("switch-namespaces", SwitchedNamespaces)
+}
+
+func SwitchedNamespaces() {
+	fmt.Printf("Restarted with switched namespaces.\n")
+
+}
+
 func main() {
-	fmt.Printf("Started and stopped.")
+	if reexec.Init() {
+		fmt.Printf("Restarted child started and done.\n")
+	} else {
+		fmt.Printf("Started.\n")
+		cmd := reexec.Command("switch-namespaces")
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Start(); err != nil {
+			panic(err)
+		} else if err := cmd.Wait(); err != nil {
+			panic(err)
+		}
+		fmt.Printf("Stopped.\n")
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/thediveo/gons
 go 1.13
 
 require (
+	github.com/moby/moby v1.13.1
 	github.com/onsi/ginkgo v1.10.1
 	github.com/onsi/gomega v1.7.0
 )
